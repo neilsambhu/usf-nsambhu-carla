fb6835125339a7fcf886f3db117d4635d0e130e6
Jennifer Adorno <jadorno@me.com>
2023-03-15 00:28:01 -0400

    moved-cache

--- build/install.sh
+++ build/install.sh
@@ -3,7 +3,7 @@
 echo [exe] rm -rf /usr/src/app/UnrealEngine
 rm -rf /usr/src/app/UnrealEngine
 cd /usr/src/app/
-7z x /usr/src/install/UnrealEngine.zip
+7z x /usr/src/cache/UnrealEngine.zip
 
 cd /usr/src/app/UnrealEngine
 echo [exe] Setup.sh
@@ -17,13 +17,14 @@ bash Engine/Build/BatchFiles/Linux/Build.sh ShaderCompileWorker Linux Developmen
 
 rm -rfv /usr/src/app/carla
 cd /usr/src/app/
-7z x /usr/src/install/carla.zip
+7z x /usr/src/cache/carla.zip
 
 export UE4_ROOT=/usr/src/app/UnrealEngine
 
 cd /usr/src/app/carla
 git checkout 0.9.13
 git checkout f628f204b44ca5f416e4d54eedfea3f9dc4a86c4 Util/BuildTools/Setup.sh
+cp -R /usr/src/cache/Content.tar.gz /usr/src/app/Content.tar.gz
 echo [exe] Update.sh
 ./Update.sh
 echo [exe] make -j 4 PythonAPI
