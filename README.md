adc7caa5dde6ea077e3b004b7d591fcc05714103
Jennifer Adorno <jadorno@users.noreply.github.com>
2024-04-18 15:05:10 +0000

    extract-assets-rename-cache

--- README.md
+++ README.md
@@ -12,12 +12,13 @@ docker build -t usf-nsambhu-carla /mnt/storage00/research/usf-nsambhu-carla
 ### Downloading assets
 
 ```
-mkdir -v usf-nsambhu-carla/binary
-cd usf-nsambhu-carla/binary
+mkdir -v usf-nsambhu-carla/cache
+cd usf-nsambhu-carla/cache
 wget  -O UnrealEngine-carla.zip "https://github.com/CarlaUnreal/UnrealEngine/archive/refs/heads/carla.zip"
 wget -O CARLA_0.9.14.zip "https://github.com/carla-simulator/carla/archive/refs/tags/0.9.14.zip"
 wget "https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/AdditionalMaps_0.9.14.tar.gz"
 wget "https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/CARLA_0.9.14_RSS.tar.gz"
+wget "https://carla-assets.s3.us-east-005.backblazeb2.com/20221201_5ec9328.tar.gz"
 ```
 
 
--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 cd /usr/src/app
 echo [exe] rm -rfv UnrealEngine-carla
 rm -rfv UnrealEngine-carla
-echo [exe] unzip binary/UnrealEngine-carla.zip
-unzip binary/UnrealEngine-carla.zip
+echo [exe] unzip cache/UnrealEngine-carla.zip
+unzip cache/UnrealEngine-carla.zip
 
 cd /usr/src/app/UnrealEngine-carla
 echo [exe] Setup.sh
@@ -19,23 +19,25 @@ bash Engine/Build/BatchFiles/Linux/Build.sh ShaderCompileWorker Linux Developmen
 cd /usr/src/app
 echo [exe] rm -rfv carla-simulator
 rm -rfv carla-simulator
-echo [exe] unzip binary/CARLA_0.9.14.zip
-unzip binary/CARLA_0.9.14.zip
+echo [exe] unzip cache/CARLA_0.9.14.zip
+unzip cache/CARLA_0.9.14.zip
 echo [exe] mv -v carla-0.9.14 carla-simulator
 mv -v carla-0.9.14 carla-simulator
 
 export UE4_ROOT=/usr/src/app/UnrealEngine-carla
 cd /usr/src/app/carla-simulator
-echo [exe] Update.sh
-./Update.sh
+# echo [exe] Update.sh
+# ./Update.sh
+echo [exe] tar -xvzf ../cache/20221201_5ec9328.tar.gz -C Unreal/CarlaUE4/Content/Carla
+tar -xvzf ../cache/20221201_5ec9328.tar.gz -C Unreal/CarlaUE4/Content/Carla
 echo [exe] make -j 4 PythonAPI
 make -j 4 PythonAPI
 echo [exe] make -j 4 launch
 make -j 4 launch
 
-echo [exe] cp -v ../binary/AdditionalMaps_0.9.14.tar.gz Import/AdditionalMaps_0.9.14.tar.gz
-cp -v ../binary/AdditionalMaps_0.9.14.tar.gz Import/AdditionalMaps_0.9.14.tar.gz
-echo [exe] cp -v ../binary/CARLA_0.9.14_RSS.tar.gz Import/CARLA_0.9.14_RSS.tar.gz
-cp -v ../binary/CARLA_0.9.14_RSS.tar.gz Import/CARLA_0.9.14_RSS.tar.gz
+echo [exe] cp -v ../cache/AdditionalMaps_0.9.14.tar.gz Import/AdditionalMaps_0.9.14.tar.gz
+cp -v ../cache/AdditionalMaps_0.9.14.tar.gz Import/AdditionalMaps_0.9.14.tar.gz
+echo [exe] cp -v ../cache/CARLA_0.9.14_RSS.tar.gz Import/CARLA_0.9.14_RSS.tar.gz
+cp -v ../cache/CARLA_0.9.14_RSS.tar.gz Import/CARLA_0.9.14_RSS.tar.gz
 echo [exe] bash ImportAssets.sh
 bash ImportAssets.sh
