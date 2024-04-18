fe536a57384224f2c278768a1f16f46d72e9d938
Jennifer Adorno <jadorno@users.noreply.github.com>
2024-04-18 14:30:09 +0000

    update-0.9.14-testing

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 # /bin/bash
 
-echo [exe] rm -rf /usr/src/app/UnrealEngine
-rm -rf /usr/src/app/UnrealEngine
-cd /usr/src/app/
-git clone --depth 1 -b carla git@github.com:CarlaUnreal/UnrealEngine.git
+cd /usr/src/app
+echo [exe] rm -rfv UnrealEngine-carla
+rm -rfv UnrealEngine-carla
+echo [exe] unzip binary/UnrealEngine-carla.zip
+unzip binary/UnrealEngine-carla.zip
 
-cd /usr/src/app/UnrealEngine
+cd /usr/src/app/UnrealEngine-carla
 echo [exe] Setup.sh
 ./Setup.sh
 echo [exe] GenerateProjectFiles.sh
@@ -15,18 +16,26 @@ make -j 4
 echo [exe] bash Engine/Build/BatchFiles/Linux/Build.sh ShaderCompileWorker Linux Development
 bash Engine/Build/BatchFiles/Linux/Build.sh ShaderCompileWorker Linux Development
 
-rm -rfv /usr/src/app/carla
-cd /usr/src/app/
-git clone https://github.com/carla-simulator/carla
+cd /usr/src/app
+echo [exe] rm -rfv carla-simulator
+rm -rfv carla-simulator
+echo [exe] unzip binary/CARLA_0.9.14.zip
+unzip binary/CARLA_0.9.14.zip
+echo [exe] mv -v carla-0.9.14 carla-simulator
+mv -v carla-0.9.14 carla-simulator
 
-export UE4_ROOT=/usr/src/app/UnrealEngine
-
-cd /usr/src/app/carla
-git checkout 0.9.13
-sed -i 's~XERCESC_REPO=https://ftp.cixug.es/apache//xerces/c/3/sources/xerces-c-${XERCESC_VERSION}.tar.gz~XERCESC_REPO=https://archive.apache.org/dist/xerces/c/3/sources/xerces-c-${XERCESC_VERSION}.tar.gz~g' Util/BuildTools/Setup.sh
+export UE4_ROOT=/usr/src/app/UnrealEngine-carla
+cd /usr/src/app/carla-simulator
 echo [exe] Update.sh
 ./Update.sh
 echo [exe] make -j 4 PythonAPI
 make -j 4 PythonAPI
 echo [exe] make -j 4 launch
 make -j 4 launch
+
+echo [exe] cp -v ../binary/AdditionalMaps_0.9.14.tar.gz Import/AdditionalMaps_0.9.14.tar.gz
+cp -v ../binary/AdditionalMaps_0.9.14.tar.gz Import/AdditionalMaps_0.9.14.tar.gz
+echo [exe] cp -v ../binary/CARLA_0.9.14_RSS.tar.gz Import/CARLA_0.9.14_RSS.tar.gz
+cp -v ../binary/CARLA_0.9.14_RSS.tar.gz Import/CARLA_0.9.14_RSS.tar.gz
+echo [exe] bash ImportAssets.sh
+bash ImportAssets.sh
